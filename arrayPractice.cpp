8966bb333198f9aab20f1232e21395435512792e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-01-06 00:00:00 +0530

    Day 1

--- arrayPractice.cpp
+++ arrayPractice.cpp
@@ -1,77 +1,87 @@
-// // Create an Integer Array (Student) with size of 56
-#include<bits/stdc++.h>
-#include<cstring>
-using namespace std;
+// // // Create an Integer Array (Student) with size of 56
+// #include<bits/stdc++.h>
+// #include<cstring>
+// using namespace std;
+// // int main(){
+// //     int student_marks[56]={99,94,92,79,86};
+// //     for (int i = 0; i < 5; i++)
+// //     {
+// //         cout<<student_marks[i]<<" "<<endl;
+// //     }
+// //     for(int j=0 ;j< 5;j++){
+// //         cout<<"The Double of "<<student_marks[j]<<" is : "<<2*student_marks[j]<<endl;
+// //     }
+    
+    
+// // }
+// // bool find(int arr[], int size, int key){
+//     //     for (int i = 0; i < size; i++)
+//     //     {
+//     //         if(arr[i]==key){
+//     //             return true;
+//     //         }
+//     //     }
+//     //     return false;
+        
+//     // }
+    
+    
+
 // int main(){
-//     int student_marks[56]={99,94,92,79,86};
-//     for (int i = 0; i < 5; i++)
-//     {
-//         cout<<student_marks[i]<<" "<<endl;
-//     }
-//     for(int j=0 ;j< 5;j++){
-//         cout<<"The Double of "<<student_marks[j]<<" is : "<<2*student_marks[j]<<endl;
-//     }
-    
-    
-// }
-// bool find(int arr[], int size, int key){
-    //     for (int i = 0; i < size; i++)
-    //     {
-    //         if(arr[i]==key){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-        
-    // }
-    
-    
-
-int main(){
-    // int arr[5]= {1,2,3,4,5};
+//     // int arr[5]= {1,2,3,4,5};
     // memset(arr, 0,sizeof(arr)); //replace value by 0 using memset function
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     cout<<arr[i]<<" "; 
-    // }
-    int n;
-    int zeros = 0;
-    int ones=0;
-    int arr[]={1,7,0,4,1,5,1,1,8,0,2,9,0};
+//     // for (int i = 0; i < 5; i++)
+//     // {
+//     //     cout<<arr[i]<<" "; 
+//     // }
+//     // int n;
+//     // int zeros = 0;
+//     // int ones=0;
+//     int arr[]={1,7,0,4,1,5,1,1,8,18,0,2,9,0};
+//     int max = arr[0];
+//     // Maximum No.
+//     for (int i = 1; i < sizeof(arr)/sizeof(int); i++)
+//     {
+//         if(max<arr[i]){
+//             max= arr[i];
+//         }
+//     }
+//     cout<<max<<endl;
 
-    for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
-    {
-        if (arr[i] == 0)
-        {
-            zeros++;
-        }
-        if(arr[i]==1){
-            ones++;
-        }
+//     // for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
+//     // {
+//     //     if (arr[i] == 0)
+//     //     {
+//     //         zeros++;
+//     //     }
+//     //     if(arr[i]==1){
+//     //         ones++;
+//     //     }
         
-    }
-    cout<<zeros<<endl;
-    cout<<ones<<endl; 
+//     // }
+//     // cout<<zeros<<endl;
+//     // cout<<ones<<endl; 
 
-    // for (int i = 0; i < 7; i++)
-    // {
-    //     if(arr[i]==n){
-    //         cout<<"Present in "<<i<<" index";
-    //         break;
-    //     }
-    //     else
-    //     {
-    //         continue;
-    //     }
-    // }
+//     // for (int i = 0; i < 7; i++)
+//     // {
+//     //     if(arr[i]==n){
+//     //         cout<<"Present in "<<i<<" index";
+//     //         break;
+//     //     }
+//     //     else
+//     //     {
+//     //         continue;
+//     //     }
+//     // }
     
-    // bool flag = find(arr, 7, n);
-    // if (flag )
-    // {
-    //     cout<<"found";
-    // }
-    // else
-    // {
-    //     cout<<"absent";
-    // }
-}
+//     // bool flag = find(arr, 7, n);
+//     // if (flag )
+//     // {
+//     //     cout<<"found";
+//     // }
+//     // else
+//     // {
+//     //     cout<<"absent";
+//     // }
+// }
+
